ed457c46ba19764595add662d2b407adace84c98
Akshat Bagla <120774928+bagla0@users.noreply.github.com>
2025-06-26 20:04:54 -0400

    stress at path coordinates

--- opensg/__init__.py
+++ opensg/__init__.py
@@ -3,7 +3,9 @@ from opensg.solve import  compute_timo_boun, compute_solidtimo_boun, compute_sti
 from opensg.io import load_yaml, write_yaml
 from opensg.mesh import BladeMesh
 
-from opensg.compute_utils import generate_boundary_markers,Rsig,C,mass_boun,compute_nullspace,dof_mapping_quad,\
-            solve_ksp, compute_nullspace, gamma_e, gamma_h, gamma_l, local_boun,initialize_array,epsilon, sigma,sigma_prestress
-
+from opensg.compute_utils import generate_boundary_markers,Rsig,C,mass_boun,compute_nullspace,dof_mapping_quad, recov,\
+            solve_ksp, compute_nullspace, gamma_e, gamma_h, gamma_l, local_boun,initialize_array,epsilon, sigma,sigma_prestress\
+            ,EPS_get_spectrum,solve_GEP_shiftinvert, CC, stress_output
+            
+from opensg.stress_recov import beam_reaction, recover_local_strain, eigen_stiffness_matrix, local_stress
 __version__ = "0.0.1"
